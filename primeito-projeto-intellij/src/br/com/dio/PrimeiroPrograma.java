8b36fd937e771f8d9422017c6eda72927eb698f4
Mauricio <mauriciofagner2010@hotmail.com>
2023-02-21 12:30:12 -0300

    Criar classe Livro para testar a construção

--- primeito-projeto-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ primeito-projeto-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -1,14 +1,18 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
+        Livro livro = new Livro("Vidas Secas", 250);
+
         System.out.println(gato);
+        System.out.println(livro);
 
         int a = 5;
-        int b = 3;
+        int b = 5;
         System.out.println("Hello World " + (a + b));
     }
 }
