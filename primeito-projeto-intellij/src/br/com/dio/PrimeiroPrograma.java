2caa60a7aeadb06e1e67eb5f0c379ada55424bc1
Mauricio <mauriciofagner2010@hotmail.com>
2023-02-21 12:21:47 -0300

    Ajustar a formatação do código (ctrl+alt+l)

--- primeito-projeto-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ primeito-projeto-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -4,11 +4,11 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-       Gato gato = new Gato();
-       System.out.println(gato);
+        Gato gato = new Gato();
+        System.out.println(gato);
 
-       int a = 5;
-       int b = 3;
-       System.out.println("Hello World " + (a+b));
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello World " + (a + b));
     }
 }
