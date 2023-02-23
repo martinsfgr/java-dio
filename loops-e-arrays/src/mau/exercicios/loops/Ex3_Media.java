5122de7b06e293d704c1c77b05e28a8e28974134
Mauricio <mauriciofagner2010@hotmail.com>
2023-02-23 10:55:00 -0300

    Resolver o exercício 3 de loops

---
+++ loops-e-arrays/src/mau/exercicios/loops/Ex3_Media.java
@@ -0,0 +1,28 @@
+package mau.exercicios.loops;
+
+import java.util.Scanner;
+
+/* Faça um programa que leia 5 números e informe o maior número e a média desses números. */
+
+
+public class Ex3_Media {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int soma = 0;
+        int maior = 0;
+        int count = 0;
+
+        do {
+            System.out.println("Número:");
+            numero = scan.nextInt();
+            maior = maior < numero ? numero : maior; // Math.max(maior, numero);
+            soma = soma + numero;
+            count++;
+        } while (count < 5);
+
+        System.out.println("Maior número: " + maior);
+        System.out.println("Média: " + soma/5);
+    }
+}
