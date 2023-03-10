16e5f762673f5e0e12bbe400d513ceabadb4ec19
Mauricio <mauriciofagner2010@hotmail.com>
2023-03-10 10:49:10 -0300

    Refatorar a classe No com generics

--- estrutura-de-dados/src/com/no/Main.java
+++ estrutura-de-dados/src/com/no/Main.java
@@ -3,18 +3,19 @@ package com.no;
 public class Main {
 
     public static void main(String[] args) {
-        No no1 = new No("Conteúdo no1");
+        No<String> no1 = new No<>("Conteúdo no1");
 
-        No no2 = new No("Conteúdo no2");
+        No<String> no2 = new No<>("Conteúdo no2");
         no1.setProximoNo(no2);
 
-        No no3 = new No("Conteúdo no3");
+        No<String> no3 = new No<>("Conteúdo no3");
         no2.setProximoNo(no3);
 
-        No no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<>("Conteúdo no4");
         no3.setProximoNo(no4);
 
         System.out.println(no1);
         System.out.println(no1.getProximoNo());
+        System.out.println(no4.getProximoNo());
     }
 }
--- estrutura-de-dados/src/com/no/No.java
+++ estrutura-de-dados/src/com/no/No.java
@@ -1,23 +1,23 @@
 package com.no;
 
-public class No {
-    private String conteudo;
-    private No proximoNo;
+public class No<T> {
+    private T conteudo;
+    private No<T> proximoNo;
 
-    public No(String conteudo) {
+    public No(T conteudo) {
         this.proximoNo = null;
         this.conteudo = conteudo;
     }
 
-    public String getConteudo() {
+    public T getConteudo() {
         return conteudo;
     }
 
-    public void setConteudo(String conteudo) {
+    public void setConteudo(T conteudo) {
         this.conteudo = conteudo;
     }
 
-    public No getProximoNo() {
+    public No<T> getProximoNo() {
         return proximoNo;
     }
 
