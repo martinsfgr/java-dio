f9c00ea59088312bf280e50de2a4f9bf42b86e12
Mauricio <mauriciofagner2010@hotmail.com>
2023-05-17 23:34:04 -0300

    Implementar remove na Lista Encadeada

--- estrutura_de_dados/src/com/lista_encadeada/ListaEncadeada.java
+++ estrutura_de_dados/src/com/lista_encadeada/ListaEncadeada.java
@@ -58,6 +58,20 @@ public class ListaEncadeada<T> {
         return noRetorno;
     }
 
+    public T remove(int index) {
+        No<T> noPivo = this.getNo(index);
+
+        if (index == 0) {
+            referenciaEntrada = noPivo.getProximoNo();
+            return noPivo.getConteudo();
+        }
+
+        No<T> noAnterior = getNo(index-1);
+        noAnterior.setProximoNo(noPivo.getProximoNo());
+
+        return noPivo.getConteudo();
+    }
+
     public T get(int index) {
         return getNo(index).getConteudo();
     }
