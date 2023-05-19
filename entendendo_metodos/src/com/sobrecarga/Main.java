400d3667db8b77bd83395775babfe76975de2d63
Mauricio <mauriciofagner2010@hotmail.com>
2023-05-19 17:08:04 -0300

    Adicionar losando no cálculo de quadriláteros especificando o tipo de dado

--- entendendo_metodos/src/com/sobrecarga/Main.java
+++ entendendo_metodos/src/com/sobrecarga/Main.java
@@ -3,7 +3,8 @@ package entendendo_metodos.src.com.sobrecarga;
 public class Main {
     public static void main(String[] args) {
         Quadrilatero.area(3);
-        Quadrilatero.area(3, 4);
+        Quadrilatero.area(3d, 4d);
         Quadrilatero.area(3, 4, 5);
+        Quadrilatero.area(3f, 4f);
     }
 }
--- entendendo_metodos/src/com/sobrecarga/Quadrilatero.java
+++ entendendo_metodos/src/com/sobrecarga/Quadrilatero.java
@@ -2,14 +2,18 @@ package entendendo_metodos.src.com.sobrecarga;
 
 public class Quadrilatero {
     public static void area (double lado) {
-        System.out.println("Área do quadrado: " + lado * lado);
+        System.out.println("Área do quadrado: " + lado*lado);
     }
 
     public static void area (double ladoUm, double ladoDois) {
-        System.out.println("Área do retângulo: " + ladoUm * ladoDois);
+        System.out.println("Área do retângulo: " + ladoUm*ladoDois);
     }
 
     public static void area (double baseMaior, double baseMenor, double altura) {
         System.out.println("Área do trapézio: " + ((baseMaior+baseMenor)*altura)/2);
+    }
+
+    public static void area (float diagonalUm, float diagonalDois) {
+        System.out.println("Área do losango: " + (diagonalUm*diagonalDois)/2);
     }
 }
